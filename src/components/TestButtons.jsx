55d9e0ab66243fa6d7dfff936c588c7a58b2e991
unknown <joao_victor-08@hotmail.com>
2023-04-30 15:02:04 -0300

    Finalizado componente teste

--- src/components/TestButtons.jsx
+++ src/components/TestButtons.jsx
@@ -1,20 +1,23 @@
 import styles from './TestButtons.module.css';
 
+import { List } from 'phosphor-react';
+
 export function TestButtons(){
     return (
         <div className={styles.TestButtons}>
             <h2>Teste os botões</h2>
             <p>Interaja com os botões e observe a mudança de aparência e de cursores</p>
 
-            <button>
+            <button className={styles.primaryButton}>
                 INTERAJA COMIGO
             </button>
 
-            <button>
+            <button className={styles.secondaryButton}>
                 INTERAJA COMIGO
             </button>
 
-            <button>
+            <button className={styles.tertiaryButton}>
+                <List size={14} color="#ffffff" />
                 INTERAJA COMIGO
             </button>
         </div>
